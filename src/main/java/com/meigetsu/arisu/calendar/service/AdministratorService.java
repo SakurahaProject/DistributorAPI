680a933a64cec2117d917352224b8bad88c19ae9
Kamioda <kamiodamasato@outlook.com>
2025-10-25 10:56:05 +0900

    ID生成の不具合を修正

--- src/main/java/com/meigetsu/arisu/calendar/service/AdministratorService.java
+++ src/main/java/com/meigetsu/arisu/calendar/service/AdministratorService.java
@@ -15,8 +15,8 @@ public class AdministratorService {
 
     public String append(CreateAdministrator record) {
         long currentRecordCount = administratorRepository.count();
-        String id = String.format("%5d", currentRecordCount + 1);
-        id = String.valueOf(CheckDigit.calcModulus10Weight21(id)) + id;
+        String id = String.format("%5d", currentRecordCount + 1).replace(' ', '0');
+        id = "a" + String.valueOf(CheckDigit.calcModulus10Weight21(id)) + id;
         Administrator newAdmin = new Administrator(id, record.mailAddress(), record.password());
         administratorRepository.save(newAdmin);
         return id;
